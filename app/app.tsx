e5146c81d1f701695aeb35e407246ad8050933d2
Ashish Kapoor <swiftobjc@gmail.com>
2025-04-11 06:01:56 +0000

    add remaining bits

--- app/app.tsx
+++ app/app.tsx
@@ -2,13 +2,24 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Button } from "@/components/ui/button";
 import "@/assets/styles/globals.css";
 import { ModeToggle } from "./components/mode-toggle";
+import Typography from "./components/ui/typography";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <div className="flex h-screen items-center justify-center bg-background">
-        <Button>Click Me</Button>
+      <div className="absolute top-4 right-4">
         <ModeToggle />
+        <img
+          src="app/assets/images/logo/logo.webp"
+          alt="Vite logo"
+          className="h-8 w-8"
+        />
+      </div>
+      <div className="flex flex-col h-screen items-center justify-center bg-background">
+        <Typography variant="h1" className="text-2xl">
+          Hello World!
+        </Typography>
+        <Button>Hello</Button>
       </div>
     </ThemeProvider>
   );
